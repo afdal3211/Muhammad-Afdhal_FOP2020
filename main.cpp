ebf099065cc445a5f09e52a777b38e25a4334226
afdal3211 <afdal3211@gmail.com>
2020-09-11 02:48:35 +0000

    Program Hellow World

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; 
+int main() 
+{ 
+	//Codingan "hello world" First exercise
+	//Using "cout" to display Words
+	
+	cout<<"Hello, My Name is Muhammad Afdhal"<<endl;
+	cout<<"I'm Prasmulyan;Software Engineering 2020"<<endl;
+	cout<<"First Exercise of LAB FOP"<<endl;
+	
+	return 0;
+	
+}
